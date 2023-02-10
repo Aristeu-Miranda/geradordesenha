1be0c4b3ab0bda06aa1440d37409ae62dee4c76c
Aristeu-Miranda <aristeujose1999@gmail.com>
2023-02-10 18:24:36 -0300

    parte de estilizacao finalizada

--- src/Components/campos.jsx
+++ src/Components/campos.jsx
@@ -4,7 +4,53 @@ import { DivCampos } from "../styled/styled";
 export default function Campos() {
     return (
         <DivCampos>
-            <p>Teste de vermelho</p>
+            <div className="fundo">
+                <div className="contador">
+                    <p>Character Length</p>
+                    <p>0</p>
+                </div>
+                <div className="inputrange">
+                    <input type="range" min={0} max={25} />
+                </div>
+                <form>
+                    <div>
+                        <input type="checkbox" name="" id="uppercase" />
+                        <span className="check"></span>
+                        <label htmlFor="uppercase">Include Uppercase Latters</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id="lowercase" />
+                        <span className="check"></span>
+                        <label htmlFor="lowercase">Include Lowercase Latters</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id="checkbox-numbers" />
+                        <span className="check"></span>
+                        <label htmlFor="checkbox-numbers">Include Numbers</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id="checkbox-symbols" />
+                        <span className="check"></span>
+                        <label htmlFor="checkbox-symbols">Include Symbols</label>
+                    </div>
+                </form>
+                <div className="contorno-forca">
+                    <div className="forca">
+                        <div className="txt-forca">
+                            <p>STRENGHT</p>
+                        </div>
+                        <div className="valores-forca">
+                            <div className="valor-forca"></div>
+                            <div className="valor-forca"></div>
+                            <div className="valor-forca"></div>
+                            <div className="valor-forca"></div>
+                        </div>
+                    </div>
+                    <div className="generate">
+                        <button type="submit">GENERATE</button>
+                    </div>
+                </div>
+            </div>
         </DivCampos>
     )
 }
--- src/styled/styled.js
+++ src/styled/styled.js
@@ -51,7 +51,6 @@ form {
     width: 15%;
     height: 80%;
     background-color: #282631;
-    margin-top: 5px;
     vertical-align: top;
     border-left: 1px solid #ADFFB3;
     cursor: pointer;
@@ -115,8 +114,160 @@ form {
 `
 
 export const DivCampos = styled.div`
+.fundo {
+    width: 100%;
+    height: 398px;
+}
+
+.contador {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 25px;
+    background-color: #282631;
+    padding: 20px;  
+}
+
 p {
-    color: red;
+    background-color: #282631;
+}
+
+.contador > p:first-of-type {
+    color: #fff;
+    font-size: 1em;
+    font-weight: 700;
+    line-height: 21.12px;
+    margin-right: 215px;
+    padding-top: 10px;
+}
+
+.contador > p:last-of-type {
+    color: #ADFFB3;
+    font-weight: 400;
+    font-size: 2em;
+    line-height: 42.24px;
+}
+
+.inputrange {
+    background-color: #282631;
+    padding: 0 30px 0 25px;
+}
+
+.inputrange > input {
+    -webkit-appearance: none;
+    width: 100%;
+    background-color: #161323;
+    height: 5px;
+    vertical-align: top;
+    cursor: pointer;
+}
+
+.inputrange > input::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 16px;
+    height: 16px;
+    background-color: #D9D9D9;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+form {
+    padding-top: 30px;
+    background-color: #282631;
+}
+
+form > div {
+    width: 100%;
+    background-color: #282631;
+    padding-left: 25px;
+    margin-bottom: 10px;
+}
+
+form > div:last-of-type {
+    margin-bottom: 0px;
+}
+
+form > div > input {
+    width: 14px;
+    height: 14px;
+    accent-color: #282631;
+}
+
+label {
+    background-color: #282631;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 18.48px;
+    vertical-align: top;
+    margin-left: 10px;
+}
+
+.contorno-forca {
+    padding: 30px 25px 0px 25px;
+    background-color: #282631;
+    height: 140px;
+}
+
+.forca {
+    background-color: #161323;
+    display: flex;
+    justify-content: space-around;
+    height: 41px;
+}
+
+.txt-forca{
+    display:inline-block;
+    background-color: #161323;
+    color: #fff;
+    margin-right: 100px;
+    padding-top: 12px;
+}
+
+.txt-forca > p {
+    background-color: #161323;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 15.84px;
+}
+
+.valores-forca {
+    background-color: #161323;
+    width: 45px;
+    margin-left: 100px;
+    display: flex;
+    justify-content: space-between;
+    padding: 11px 5px 5px 5px;
+}
+
+.valor-forca {
+    background-color: #161323;
+    border: 1px solid #fff;
+    width: 6px;
+    height: 17px;
+}
+
+.generate {
+    width: 100%;
+    height: 41px;
+    margin-top: 15px;
+}
+
+.generate > button {
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: #ADFFB3;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 15.84px;
+    color: #000000;
+    cursor: pointer;
+    transition: 0.5s;
+}
+
+.generate > button:hover {
+    background-color: #70B075;
 }
 `
 
